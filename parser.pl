d0dd59abb22181db2e01bb958a503fb960b1a2f8
Tobias Kirschstein <lev@ideegeo.com>
2017-05-12 16:50:24 +1200

    finished parser

--- parser.pl
+++ parser.pl
@@ -11,16 +11,12 @@ use Data::Printer;
 use File::Monitor;
 use File::Basename qw/basename/;
 use File::Path 'make_path';
+use Date::Parse;
+use Store::CouchDB;
 
 my $filename = $ARGV[0];
-
-my $city;
-open(my $fh, '<:encoding(UTF-8)', $filename)
-    or die "Could not open file '$filename' $!";
-my @files = <$fh>;
-chomp @files;
-
-my $yada = YADA->new(
+my $monitor  = File::Monitor->new();
+my $yada     = YADA->new(
     common_opts => {
 
         # Available opts @ http://curl.haxx.se/libcurl/c/curl_easy_setopt.html
@@ -31,8 +27,59 @@ my $yada = YADA->new(
     http_response => 1,
     max           => 5,
 );
+my $sc = Store::CouchDB->new(
+    db    => 'realestate_agents',
+    debug => 1,
+);
 
-my $monitor = File::Monitor->new();
+sub filter_date {
+    return str2time(join('-', reverse split(m[/], shift)));
+}
+
+sub filter_date_period {
+    my ($start, $end) = split(/ - /, shift);
+    return { start => filter_date($start), end => filter_date($end) };
+}
+
+sub filter_address {
+    my @lines = split(m[\s*<br/>\s*], shift);
+    my ($city, $pcode) = $lines[-1] =~ m/^(.*)\s+(\d+)$/;
+    my $address = {
+        street => $lines[0],
+        city   => $city,
+        pcode  => $pcode,
+    };
+    $address->{suburb} = $lines[1] if (scalar @lines > 2);
+    return $address;
+}
+
+sub filter_licencees {
+    my ($value) = @_;
+
+    # remove empy lines
+    $value =~ s/^\s*\n$//gs;
+
+    # trim remaining
+    $value =~ s/^\s+|\s+$//g;
+
+    # split around newlines
+    my @fields = split(/\s*\n\s*/, $value);
+    my @licencees;
+    push(
+        @licencees, {
+            name           => shift @fields,
+            licence_number => shift @fields,
+            branche        => shift @fields,
+        }) for (1 .. int(scalar @fields / 3));
+    return \@licencees;
+}
+
+my $city;
+open(my $fh, '<:encoding(UTF-8)', $filename)
+    or die "Could not open file '$filename' $!";
+my @files = <$fh>;
+chomp @files;
+
 $monitor->watch({
         name     => './links/' . $_,
         callback => {
@@ -55,31 +102,140 @@ $monitor->watch({
                             or not $self->response->is_success
                             or not $self->response->content_is_html;
 
-                        # Declare the scraper once and then reuse it
-                        # state $scraper = scraper {
-                        #     process 'html title', title     => 'text';
-                        #     process 'a',          'links[]' => '@href';
-                        # };
-
-                        # Employ amazing Perl (en|de)coding powers to handle HTML charsets
-                        # my $doc = $scraper->scrape($self->response->decoded_content,
-                        #     $self->final_url,);
-
-                        my ($license_id) = $self->final_url =~ /licenceid=([^&]+)&/;
-                        p $license_id;
-                        my $outfile = "./raw/$city/$license_id.html";
+                        # save raw HTML as backup
+                        my ($licence_id) =
+                            $self->final_url =~ /licenceid=([^&]+)&/;
+                        say "$city: $licence_id";
+                        my $outfile = "./raw/$city/$licence_id.html";
                         open(my $fh, ">", $outfile)
                             or die "Could not open $outfile: $!";
                         print $fh $self->response->decoded_content;
                         close($fh);
 
-                        # printf qq(%-64s %s\n), $self->final_url, $doc->{title};
+                        # Declare the scraper once and then reuse it
+                        state $scraper = scraper {
+
+                            # individual agent fields
+                            process
+                                '#ContentContainer_MainContent_FirstNameLabel',
+                                first_name => 'text';
+                            process
+                                '#ContentContainer_MainContent_MiddleNamesLabel',
+                                middle_name => 'text';
+                            process
+                                '#ContentContainer_MainContent_SurnameLabel',
+                                last_name => 'text';
+                            process
+                                '#ContentContainer_MainContent_FormernameLabel',
+                                former_name => 'text';
+                            process
+                                '#ContentContainer_MainContent_AlsoKnownAsLabel',
+                                preferred_name => 'text';
+                            process '#ContentContainer_MainContent_PhoneLabel',
+                                phone => 'text';
+                            process '#ContentContainer_MainContent_EmailLabel',
+                                email => [ 'text', sub { lc } ];
+                            process
+                                '#ContentContainer_MainContent_BusinessNameLabel',
+                                company => 'text';
+                            process
+                                '#ContentContainer_MainContent_AddressLabel',
+                                address => [ 'html', \&filter_address ];
+
+                            # company fields
+                            process '#ContentContainer_MainContent_NameLabel',
+                                company => 'text';
+                            process
+                                '#ContentContainer_MainContent_TradingNameLabel',
+                                trading_name => 'text';
+                            process
+                                '#ContentContainer_MainContent_FranchisegroupLabel',
+                                franchise => 'text';
+                            process
+                                '#ContentContainer_MainContent_BusinessAddressLabel',
+                                address => [ 'html', \&filter_address ];
+                            process
+                                '#ContentContainer_MainContent_RegisteredOfficeAddressLabel',
+                                registered_office_address =>
+                                [ 'html', \&filter_address ];
+                            process '#', bla => 'text';
+                            process '#', bla => 'text';
+                            process '#', bla => 'text';
+                            process '#', bla => 'text';
+
+                            # licence fields
+                            process
+                                '#ContentContainer_MainContent_TypeOfRealEstateWorkLabel',
+                                work_type => [ 'text', sub { lc } ];
+                            process
+                                '#ContentContainer_MainContent_ctl00_LicenceNumberLabel',
+                                licence_number => 'text';
+                            process
+                                '#ContentContainer_MainContent_LicenceNumberLabel',
+                                licence_number => 'text';
+                            process
+                                '#ContentContainer_MainContent_ctl00_LicenceTypeLabel',
+                                licence_type => [ 'text', sub { lc } ];
+                            process
+                                '#ContentContainer_MainContent_LicenceTypeLabel',
+                                licence_type => [ 'text', sub { lc } ];
+                            process
+                                '#ContentContainer_MainContent_ctl00_LicenceClassLabel',
+                                licence_class => [ 'text', sub { lc } ];
+                            process
+                                '#ContentContainer_MainContent_LicenceClassLabel',
+                                licence_class => [ 'text', sub { lc } ];
+                            process
+                                '#ContentContainer_MainContent_ctl00_CurrentStatusLabel',
+                                licence_status => [ 'text', sub { lc } ];
+                            process
+                                '#ContentContainer_MainContent_CurrentStatusLabel',
+                                licence_status => [ 'text', sub { lc } ];
+                            process
+                                '#ContentContainer_MainContent_ctl00_DateFirstEnteredLabel',
+                                licence_created => [ 'text', \&filter_date ];
+                            process
+                                '#ContentContainer_MainContent_DateFirstEnteredLabel',
+                                licence_created => [ 'text', \&filter_date ];
+                            process
+                                '#ContentContainer_MainContent_ctl00_CurrentPeriodLabel',
+                                licence_period =>
+                                [ 'text', \&filter_date_period ];
+                            process
+                                '#ContentContainer_MainContent_CurrentPeriodLabel',
+                                licence_period =>
+                                [ 'text', \&filter_date_period ];
+                            process
+                                '#ContentContainer_MainContent_ctl00_ExpiryDateLabel',
+                                licence_expire => [ 'text', \&filter_date ];
+                            process
+                                '#ContentContainer_MainContent_ExpiryDateLabel',
+                                licence_expire => [ 'text', \&filter_date ];
+                            process
+                                '#ContentContainer_MainContent_LicenceesGridView > tbody',
+                                licencees => [ 'text', \&filter_licencees ];
+
+                            # complaints history fields
+                            process
+                                '#ContentContainer_MainContent_NoDisciplinaryHistory',
+                                disciplinary_history => ['text', sub { s/^\s+|\s+$//g } ];
+                        };
+
+                        # Employ amazing Perl (en|de)coding powers to handle HTML charsets
+                        my $doc =
+                            $scraper->scrape($self->response->decoded_content,
+                            $self->final_url);
+
+                        # store in couchdb
+                        $doc->{_id}       = $doc->{licence_number};
+                        $doc->{reaa_link} = $self->final_url->as_string;
+                        my @res = $sc->put_doc({ doc => $doc });
+                        p @res;
                     })->wait;
             },
         },
     }) for @files;
 $monitor->scan;
-p $monitor;
 
 while (1) {
     sleep 1;
